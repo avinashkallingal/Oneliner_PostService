d8682c3d5d9738cf724789f24e9fa936f33dd1ad
Avinash Kumar A <90566941+avinashkallingal@users.noreply.github.com>
2024-12-30 03:28:10 +0530

    feat:hosted

--- src/infrastructure/http/server.ts
+++ src/infrastructure/http/server.ts
@@ -1,6 +1,7 @@
 import express from 'express';
 import { databaseConnection } from '../database/mongodb'
 import RabbitMQClient from '../rabbitMQ/client';
+import config from '../config/config';
 
 const app = express();
 app.use(express.json());
@@ -9,7 +10,10 @@ const startServer = async () => {
     try {
         await databaseConnection();
         await RabbitMQClient.initialize();
-        app.listen(4002, () => console.log(`postService running on port 4002`))
+        // app.listen(4002, () => console.log(`postService running on port 4002`))
+        app.listen(config.port, () => {
+            console.log(`Server is running on port ${config.port}`);
+        })
 
     } catch (error) {
         console.log('post srevice running error -->', error); 
